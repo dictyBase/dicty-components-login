f4ab8b3c4d191df1364e7f9fca90d794c3785f45
wildlifehexagon <crossthebreeze@gmail.com>
2018-04-06 14:11:00 -0500

    run build

---
+++ es/components/Login.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
+import Button from "material-ui/Button";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import brands from "@fortawesome/fontawesome-free-brands";
+import { IconContainer, socialStyles } from "../styles/Buttons";
+
+var generateLoginButtons = function generateLoginButtons(buttons, onClick) {
+  return buttons.map(function (name, i) {
+    return React.createElement(
+      Button,
+      {
+        style: { backgroundColor: "" + socialStyles[name] },
+        onClick: function (_onClick) {
+          function onClick() {
+            return _onClick.apply(this, arguments);
+          }
+
+          onClick.toString = function () {
+            return _onClick.toString();
+          };
+
+          return onClick;
+        }(function () {
+          onClick(name);
+        }),
+        key: i },
+      React.createElement(
+        IconContainer,
+        null,
+        name === "orcid" ? React.createElement("i", { className: "ai ai-orcid" }) : React.createElement(FontAwesomeIcon, { icon: ["fab", "" + name] })
+      ),
+      "\xA0 Sign in with ",
+      name
+    );
+  });
+};
+
+/**
+ * This is the main login component that generates the user-specified social buttons with an optional custom theme.
+ */
+
+var Login = function Login(_ref) {
+  var buttons = _ref.buttons,
+      theme = _ref.theme,
+      onClick = _ref.onClick;
+
+  var newTheme = createMuiTheme(theme);
+  return React.createElement(
+    MuiThemeProvider,
+    { theme: newTheme },
+    generateLoginButtons(buttons, onClick)
+  );
+};
+
+export default Login;
---
+++ es/styles/Container.js
@@ -0,0 +1,9 @@
+var _templateObject = _taggedTemplateLiteralLoose(["\n  font-family: roboto;\n  text-align: center;\n"], ["\n  font-family: roboto;\n  text-align: center;\n"]);
+
+function _taggedTemplateLiteralLoose(strings, raw) { strings.raw = raw; return strings; }
+
+import styled from "styled-components";
+import "typeface-roboto";
+
+// basic container with Material-UI font
+export var Container = styled.div(_templateObject);
