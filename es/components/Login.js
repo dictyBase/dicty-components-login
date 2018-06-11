79cfd4f1a0951f5900e660c8a609ff28f22945aa
wildlifehexagon <crossthebreeze@gmail.com>
2018-06-11 14:42:17 -0500

    update material-ui version

--- es/components/Login.js
+++ es/components/Login.js
@@ -1,6 +1,6 @@
 import React from "react"
-import { MuiThemeProvider, createMuiTheme } from "material-ui/styles"
-import Button from "material-ui/Button"
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+import Button from "@material-ui/core/Button"
 import FontAwesome from "react-fontawesome"
 import { IconContainer, socialStyles } from "../styles/Buttons"
 import "font-awesome/css/font-awesome.min.css"
