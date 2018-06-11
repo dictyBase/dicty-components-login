f727ba45fe6b249696e8cb2ab171e1c5d5487bf0
wildlifehexagon <crossthebreeze@gmail.com>
2018-06-11 13:01:28 -0500

    update build

--- es/components/Login.js
+++ es/components/Login.js
@@ -1,40 +1,43 @@
-import React from "react";
-import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
-import Button from "material-ui/Button";
-import FontAwesomeIcon from "@fortawesome/react-fontawesome";
-import brands from "@fortawesome/fontawesome-free-brands";
-import { IconContainer, socialStyles } from "../styles/Buttons";
+import React from "react"
+import { MuiThemeProvider, createMuiTheme } from "material-ui/styles"
+import Button from "material-ui/Button"
+import FontAwesome from "react-fontawesome"
+import { IconContainer, socialStyles } from "../styles/Buttons"
+import "font-awesome/css/font-awesome.min.css"
 
 var generateLoginButtons = function generateLoginButtons(buttons, onClick) {
-  return buttons.map(function (name, i) {
+  return buttons.map(function(name, i) {
     return React.createElement(
       Button,
       {
         style: { backgroundColor: "" + socialStyles[name] },
-        onClick: function (_onClick) {
+        onClick: (function(_onClick) {
           function onClick() {
-            return _onClick.apply(this, arguments);
+            return _onClick.apply(this, arguments)
           }
 
-          onClick.toString = function () {
-            return _onClick.toString();
-          };
+          onClick.toString = function() {
+            return _onClick.toString()
+          }
 
-          return onClick;
-        }(function () {
-          onClick(name);
+          return onClick
+        })(function() {
+          onClick(name)
         }),
-        key: i },
+        key: i,
+      },
       React.createElement(
         IconContainer,
         null,
-        name === "orcid" ? React.createElement("i", { className: "ai ai-orcid" }) : React.createElement(FontAwesomeIcon, { icon: ["fab", "" + name] })
+        name === "orcid"
+          ? React.createElement("i", { className: "ai ai-orcid" })
+          : React.createElement(FontAwesome, { name: name }),
       ),
       "\xA0 Sign in with ",
-      name
-    );
-  });
-};
+      name,
+    )
+  })
+}
 
 /**
  * This is the main login component that generates the user-specified social buttons with an optional custom theme.
@@ -42,20 +45,16 @@ var generateLoginButtons = function generateLoginButtons(buttons, onClick) {
 
 var Login = function Login(_ref) {
   var buttons = _ref.buttons,
-      theme = _ref.theme,
-      onClick = _ref.onClick;
+    theme = _ref.theme,
+    onClick = _ref.onClick
 
   // $FlowFixMe
-  var newTheme = createMuiTheme(theme);
+  var newTheme = createMuiTheme(theme)
   return React.createElement(
     MuiThemeProvider,
     { theme: newTheme },
-    React.createElement(
-      "div",
-      null,
-      generateLoginButtons(buttons, onClick)
-    )
-  );
-};
+    React.createElement("div", null, generateLoginButtons(buttons, onClick)),
+  )
+}
 
-export default Login;
+export default Login
