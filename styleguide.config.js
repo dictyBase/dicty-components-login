eb47a6879961d43f679910f084ed58c060c10bca
wildlifehexagon <crossthebreeze@gmail.com>
2019-07-22 17:50:13 -0500

    chore: update more deps

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,34 +1,35 @@
 module.exports = {
   components: "src/components/*.js",
+  ignore: ["src/components/*Styles.js", "src/components/*.test.js"],
   webpackConfig: {
     module: {
       rules: [
         {
-          test: /\.jsx?$/,
+          test: /\.(js|jsx|ts|tsx)$/,
           exclude: /node_modules/,
-          loader: "babel-loader"
+          loader: "babel-loader",
         },
         {
           test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: "url-loader"
+          loader: "url-loader",
         },
         {
           test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: "file-loader"
+          loader: "file-loader",
         },
         {
           test: /\.(jpg|png)$/,
           exclude: /node_modules/,
           loader: "url-loader",
           options: {
-            limit: 25000
-          }
+            limit: 25000,
+          },
         },
         {
           test: /\.css$/,
-          use: [{ loader: "style-loader" }, { loader: "css-loader" }]
-        }
-      ]
-    }
-  }
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+  },
 }
