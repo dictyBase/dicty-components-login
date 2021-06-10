907135d16f7cbee466a355cbf44f27bbdda8dcbe
wildlifehexagon <hartline.eric@gmail.com>
2021-06-10 14:55:09 -0500

    refactor: use cjs build format

--- bundle.js
+++ bundle.js
@@ -1,26 +1,22 @@
-const fs = require("fs")
-const cp = require("child_process")
-const esbuild = require("esbuild")
-const path = require("path")
+const fs = require("fs");
+const cp = require("child_process");
+const esbuild = require("esbuild");
+const path = require("path");
 
-const src = path.join(__dirname, "src")
+const src = path.join(__dirname, "src");
 
-esbuild.build({
-  entryPoints: ["src/index.d.ts"],
-  outfile: "dist/index.js",
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  target: [
-  "chrome58",
-  "firefox55",
-  "safari12",
-  "edge15",
-  "node14"
-  ],
-  format: "esm",
-  define: {
-		'process.env.NODE_ENV': '"production"',
-	},
-  external: ["@material-ui/core", "react", "react-dom"]
-}).catch(() => process.exit(1))
+esbuild
+  .build({
+    entryPoints: ["src/index.d.ts"],
+    outfile: "dist/index.js",
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    target: ["chrome58", "firefox55", "safari12", "edge15", "node14"],
+    format: "cjs",
+    define: {
+      "process.env.NODE_ENV": '"production"',
+    },
+    external: ["@material-ui/core", "react", "react-dom"],
+  })
+  .catch(() => process.exit(1));
